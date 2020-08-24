c77bde0b2906ff39a62977fa52dc731c0aa1ad1b
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-23 22:30:23 -0500

    remove build directory from gitignore, push prod build

---
+++ client/build/precache-manifest.9199a1c29e953157d8a396bc535adfb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d5865db076a3f0a0167a4dac90ce24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f236d4b216cd154b74",
+    "url": "/static/css/main.4d5d6099.chunk.css"
+  },
+  {
+    "revision": "0fb3cd5cc7ff4dd749b4",
+    "url": "/static/js/2.c9c1fe84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9c1fe84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f236d4b216cd154b74",
+    "url": "/static/js/main.6447ae0e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
